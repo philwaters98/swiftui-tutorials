e71984629eef4fde8a422c646c6a64877b7e77b3
Phil Waters <philip.waters@saltdna.com>
2021-02-11 16:46:10 +0000

    Handling User Input

---
+++ Landmarks/Views/Landmarks/LandmarksList.swift
@@ -0,0 +1,41 @@
+//
+//  LandmarksList.swift
+//  Landmarks
+//
+//  Created by Phil Waters on 10/02/2021.
+//
+
+import SwiftUI
+
+struct LandmarksList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                ForEach(filteredLandmarks) {
+                    landmark in NavigationLink(destination: LandmarkDetail(landmark: landmark)){
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarksList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarksList()
+    }
+}
